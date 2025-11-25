bf8e9c440871f580430844ee3e31005a8e70dbb7
yaswethaap-arch <ya.swethaap@gmail.com>
2025-11-25 15:11:58 +0530

    Update general.js
    Added general user endpoints code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,79 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if(isbn){
+    res.send(books[isbn]);
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const booksKeys = [];
+  
+  if(author){
+    let i=0;
+    for(let key in books){
+        booksKeys[i++] = key;
+    }
+    let selectedBook = [];
+    
+    for(i =0; i < booksKeys.length; i++){
+        if(books[booksKeys[i]].author == author){
+            selectedBook.push(books[booksKeys[i]]);
+        }
+    }
+    res.send(selectedBook);
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const booksKeys = [];
+  
+  if(title){
+    let i=0;
+    for(let key in books){
+        booksKeys[i++] = key;
+    }
+    let selectedBook = [];
+    
+    for(i =0; i < booksKeys.length; i++){
+        if(books[booksKeys[i]].title == title){
+            selectedBook.push(books[booksKeys[i]]);
+        }
+    }
+    res.send(selectedBook);
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if(isbn){
+    for(key in books){
+        if(key == isbn){
+            res.send(books[key].reviews);
+            break;
+        }
+    }
+  }
 });
 
 module.exports.general = public_users;
